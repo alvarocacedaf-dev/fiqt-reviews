20823121372dfb04b52a61b08f7294a8e1163325
alvarocacedaf-dev <alvaro.caceda.f@uni.pe>
2026-08-29 04:07:00 -0500

    Acercar ruta academica a verificacion

--- components/Header.tsx
+++ components/Header.tsx
@@ -147,7 +147,7 @@ function LoggedInLinks({
 
   return (
     <>
-      <a className="inline-flex items-center gap-2 transition hover:text-gold" href="/ciclos">
+      <a className="inline-flex translate-x-6 items-center gap-2 transition hover:text-gold" href="/ciclos">
         <NavIcon type="academic" />
         Ruta académica
       </a>
